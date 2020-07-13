ea11674aca7f7a60b5646a03e301abb2861a20b0
admin <root@localhost>
2020-07-13 22:04:27 +0000

    changed output structure

--- custom_functions/normalize_lists.py
+++ custom_functions/normalize_lists.py
@@ -1,29 +1,29 @@
-def normalize_lists(var=None, object_type=None, **kwargs):
+def normalize_lists(input_item=None, object_type=None, **kwargs):
     """
     Takes in an object that may be a single element or a list and normalizes the output. Data can be accessed by items.*.<object_type>. Object_type defaults to 'object_type'
     
     Args:
-        var
+        input_item
         object_type
     
     Returns a JSON-serializable object that implements the configured data paths:
-        items
-        items.*.object_type
+        item
+        object_type
     """
     ############################ Custom Code Goes Below This Line #################################
     import json
     import phantom.rules as phantom
     
-    outputs = {'items': []}
+    outputs = {}
+
+    if input_item and type(input_item) == list and object_type:
+        for item in input_item:
+            outputs['item'] = 'item'
+            outputs['object_type'] = object_type
+    elif input_item and object_type:
+        outputs['item'] = input_item
+        outputs['object_type'] = object_type
     
-    if not object_type:
-        object_type = 'object_type'
-    # Write your custom code here...
-    if var and type(var) == list and object_type:
-        for item in var:
-            outputs['items'].append({object_type: item})
-    elif var and object_type:
-        outputs['items'].append({object_type: var})
         
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
