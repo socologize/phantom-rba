13b329541fc1ed3fcf6b3131ad42c2639c87a445
admin <root@localhost>
2020-08-21 16:07:50 +0000

    fixing bug with formatting

--- custom_functions/parse_risk_results.py
+++ custom_functions/parse_risk_results.py
@@ -82,13 +82,8 @@ def parse_risk_results(search_json=None, **kwargs):
                     tags.append(match[1])
 
             # Parse through threat_objects
-            # if 'threat_object' in artifact_json.keys():
-
-                #if type(artifact_json['threat_object']) == list:
-                #    for listitem in artifact_json['threat_object']:
-                #        threatlist.append(listitem)
-                #else:
-                #    threatlist.append(artifact_json['threat_object'])
+            if 'threat_object' in artifact_json.keys():
+                artifact_json['threat_object'] = json.dumps(artifact_json['threat_object'])
 
             # Extract tags
             if 'rule_attack_tactic_technique' in artifact_json.keys():
