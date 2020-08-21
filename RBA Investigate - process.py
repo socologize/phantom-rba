106df8e9774de2c8851cfedc20079e114147faa0
admin <root@localhost>
2020-08-21 12:56:46 +0000

    Saved: minor fix

--- RBA Investigate - process.py
+++ RBA Investigate - process.py
@@ -104,7 +104,15 @@ def cf_rba_master_regex_extract_powershell_b64_1(action=None, success=None, cont
     ################################################################################    
 
     # call custom function "rba-master/regex_extract_powershell_b64", returns the custom_function_run_id
-    phantom.custom_function(custom_function='rba-master/regex_extract_powershell_b64', parameters=parameters, name='cf_rba_master_regex_extract_powershell_b64_1', callback=decision_1)
+    phantom.custom_function(custom_function='rba-master/regex_extract_powershell_b64', parameters=parameters, name='cf_rba_master_regex_extract_powershell_b64_1', callback=cf_rba_master_regex_extract_powershell_b64_1_callback)
+
+    return
+
+def cf_rba_master_regex_extract_powershell_b64_1_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None):
+    phantom.debug('cf_rba_master_regex_extract_powershell_b64_1_callback() called')
+    
+    decision_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+    filter_4(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
 
     return
 
@@ -121,7 +129,6 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 1 matched
     if matched:
-        format_3(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
         return
 
     return
@@ -293,14 +300,15 @@ def filter_4(action=None, success=None, container=None, results=None, handle=Non
     # collect filtered artifact ids for 'if' condition 1
     matched_artifacts_1, matched_results_1 = phantom.condition(
         container=container,
+        action_results=results,
         conditions=[
-            ["cf_rba_master_regex_extract_powershell_b64_1:custom_function_result.data.extracted_string", "!=", ""],
+            ["cf_rba_master_regex_extract_powershell_b64_1:custom_function_result.data.artifact_id", "!=", ""],
         ],
         name="filter_4:condition_1")
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        cf_community_noop_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        format_3(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
